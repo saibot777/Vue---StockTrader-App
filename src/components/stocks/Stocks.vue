d236879891336bb23c2d93827e79fa09dea0fadd
saibot777 <stefantrajkovic777@gmail.com>
2017-03-26 13:16:16 +0200

    stocks component layout created

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,26 @@
 <template lang="html">
-  <h1>The Stocks Component</h1>
+  <div>
+      <app-stock v-for="stock in stocks"></app-stock>
+  </div>
 </template>
 
 <script>
+import Stock from './Stock.vue';
+
 export default {
+  data() {
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 200},
+        {id: 3, name: 'Apple', price: 190},
+        {id: 4, name: 'Twitter', price: 80}
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
 }
 </script>
 
