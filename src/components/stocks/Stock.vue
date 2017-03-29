28d08a6226d52852bb0fed0a30a824875322b24b
saibot777 <stefantrajkovic777@gmail.com>
2017-03-29 16:32:40 +0200

    stocks looping...

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,27 +2,25 @@
     <div class="col-sm-6 col-md-4">
         <div class="panel panel-success">
             <div class="panel-heading">
-                <h3 class="panel-title"> Stock Component
-                    <!-- {{ stock.name }}
-                    <small>(Price: {{ stock.price }})</small> -->
+                <h3 class="panel-title">
+                    NAME
+                    <small>(Price: PRICE)</small>
                 </h3>
             </div>
             <div class="panel-body">
                 <div class="pull-left">
-                    <!-- <input
+                    <input
                             type="number"
                             class="form-control"
                             placeholder="Quantity"
-                            v-model="quantity"
-                    > -->
+                    >
                 </div>
                 <div class="pull-right">
-                    <!-- <button
+                    <button
                             class="btn btn-success"
-                            @click="buyStock"
-                            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+
                     >Buy
-                    </button> -->
+                    </button>
                 </div>
             </div>
         </div>
@@ -36,23 +34,5 @@
 </style>
 
 <script>
-    // export default {
-    //     props: ['stock'],
-    //     data() {
-    //         return {
-    //             quantity: 0
-    //         }
-    //     },
-    //     methods: {
-    //         buyStock() {
-    //             const order = {
-    //                 stockId: this.stock.id,
-    //                 stockPrice: this.stock.price,
-    //                 quantity: this.quantity
-    //             }
-    //             console.log(order);
-    //                 this.quantity = 0;
-    //         }
-    //     }
-    // }
+    
 </script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,7 +1,6 @@
 <template lang="html">
   <div>
-    Hello
-      <!-- <app-stock v-for="stock in stocks" :key="stock.id"></app-stock> -->
+      <app-stock v-for="stock in stocks"></app-stock>
   </div>
 </template>
 
@@ -9,19 +8,19 @@
 import Stock from './Stock.vue'
 
 export default {
-  // data() {
-  //   return {
-  //     stocks: [
-  //       {id: 1, name: 'BMW', price: 110},
-  //       {id: 2, name: 'Google', price: 200},
-  //       {id: 3, name: 'Apple', price: 190},
-  //       {id: 4, name: 'Twitter', price: 80}
-  //     ]
-  //   }
-  // },
-  // components: {
-  //   appStock: Stock
-  // }
+  data() {
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 200},
+        {id: 3, name: 'Apple', price: 190},
+        {id: 4, name: 'Twitter', price: 80}
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
 }
 </script>
 
