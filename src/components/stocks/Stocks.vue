e8f7b3d3f7f448edd33c563dca519242032a2bff
frenzy777 <stefantrajkovic777@gmail.com>
2017-03-29 16:39:26 +0200

    fix

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -11,7 +11,7 @@ export default {
   data() {
     return {
       stocks: [
-        {id: 1, name: 'BMWs', price: 110},
+        {id: 1, name: 'BMW', price: 110},
         {id: 2, name: 'Google', price: 200},
         {id: 3, name: 'Apple', price: 190},
         {id: 4, name: 'Twitter', price: 80}
