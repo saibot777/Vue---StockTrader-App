bfcf2c442e33c5d1983083225f303c719991bd12
saibot777 <stefantrajkovic777@gmail.com>
2018-01-28 21:49:48 +0100

    rendering stocks list..

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>STOCKS</h1>
+  <div>
+    <app-stock 
+      v-for="stock in stocks" 
+      v-bind:key="stock.id">
+    </app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue';
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 60 },
+          { id: 2, name: 'Mercedes', price: 90 },
+          { id: 3, name: 'McLaren', price: 110 },
+          { id: 4, name: 'Jaguar', price: 70 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
