b07ff9f07da4dc8126d8c10a80235e5f68110963
saibot777 <stefantrajkovic777@gmail.com>
2018-01-28 22:33:17 +0100

    VUEX implemented

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -12,18 +12,13 @@
   import Stock from './Stock.vue';
 
   export default {
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: 'BMW', price: 60 },
-          { id: 2, name: 'Mercedes', price: 90 },
-          { id: 3, name: 'McLaren', price: 110 },
-          { id: 4, name: 'Jaguar', price: 70 }
-        ]
-      }
-    },
     components: {
       appStock: Stock
+    },
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks
+      }
     }
   }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'BMW', price: 60 },
+    { id: 2, name: 'Mercedes', price: 90 },
+    { id: 3, name: 'McLaren', price: 110 },
+    { id: 4, name: 'Jaguar', price: 70 }
+];
